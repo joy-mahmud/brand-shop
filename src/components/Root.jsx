e59df0d78386024825df5dad99e9cd4b1189bd8f
joy-mahmud <joymahmudiucse@gmail.com>
2023-10-21 21:49:23 +0600

    Dark and light theme has been set for the entire website

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,11 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
+import { useState } from "react";
 
 
 const Root = () => {
+    const[black,setTheme]=useState(true)
+  const handleTheme=()=>{
+        setTheme(!black)
+    }
     return (
-        <div>
+        <div style={{backgroundColor:black?"#001a23": "white", color:black?"white":''}}>
+            <div className="flex justify-end"><button onClick={handleTheme} className="btn btn-sm">{black?"Light":"Dark"}</button></div>
             <Header></Header>
             <Outlet></Outlet>
             <Footer></Footer>
